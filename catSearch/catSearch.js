a21d8bb53bee0a60bc864dc2400c50b70e104efe
ben <113296723+fluark@users.noreply.github.com>
2024-03-25 11:15:12 -0500

    Fix relative link for catSearch file

---
+++ catSearch/catSearch.js
@@ -0,0 +1,42 @@
+
+const cats = [
+    "Porkchop: Ben",
+    "Foley: Gideon",
+    "Chicken: Eva",
+    "Chalupa: Andi",
+    "Lily: Deni",
+    "Dophi: Josiah",
+    "Piper: Olga",
+    "Fig: Glenn",
+    "Ava: Glenn",
+    "Ollie: Lucy",
+];
+
+const para = document.querySelector("p");
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", handleSearch);
+ document.addEventListener("keypress", function(event){
+    if (event.key === "Enter") {
+        handleSearch();
+    }
+ });
+
+function handleSearch(){
+    const searchName = input.value.toLowerCase();
+    input.value = "";
+    input.focus();
+    para.textContent = "";
+
+    for (const cat of cats){
+        const splitCat = cat.split(":");
+        if (splitCat[0].toLowerCase() === searchName){
+            para.textContent = `${splitCat[0]}'s owner is
+            ${splitCat[1]}.`;
+            return;
+        }
+    }
+        para.textContent = "Cat not found";
+    }
+
